970aca59d48c9c08bd00979884d85a376b6caaeb
Juribiyan <smugleaf@ya.ru>
2023-04-17 23:44:07 +0500

    Restore visitors' original IPs behind Cloudflare

--- config.php
+++ config.php
@@ -24,3 +24,5 @@ set_error_handler(function(int $errno, string $errstr) {
     return true;
   }
 }, E_WARNING);
+
+if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
