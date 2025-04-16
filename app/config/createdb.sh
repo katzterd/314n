8bd24c6a5696a69e6aa002a1aaf8ba51d4e0263d
katzterd <194586003+katzterd@users.noreply.github.com>
2025-04-16 19:49:49 +0300

    config files separated from application

--- app/config/createdb.sh
+++ app/config/createdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mariadb -h$MARIADB_HOST -u$MARIADB_USER -p$MARIADB_PASSWORD $MARIADB_DATABASE < /app/config/structure.sql && printf "\nDatabase created!" || printf "\nDatabase already exists"
+mariadb -h$MARIADB_HOST -u$MARIADB_USER -p$MARIADB_PASSWORD $MARIADB_DATABASE < /src/config/structure.sql && printf "\nDatabase created!" || printf "\nDatabase already exists"
--- app/config/docker-entrypoint.sh
+++ app/config/docker-entrypoint.sh
@@ -2,18 +2,18 @@
 
   if [ -z "${TORGATE_HOSTNAME}" ]; then
       echo "TORGATE_HOSTNAME environment var is undefined, onion header will be disabled";
-      sed -i 's/__TORGATE_HEADER__//' /app/config/nginx.conf
+      sed -i 's/__TORGATE_HEADER__//' /src/config/nginx.conf
   else
-      sed -i 's/__TORGATE_HEADER__/'"add_header Onion-Location http:\/\/${TORGATE_HOSTNAME}\$request_uri;"'/' /app/config/nginx.conf
+      sed -i 's/__TORGATE_HEADER__/'"add_header Onion-Location http:\/\/${TORGATE_HOSTNAME}\$request_uri;"'/' /src/config/nginx.conf
   fi
 
   for ARGS in $@; do
 	    case $ARGS in    
-	        "createdb")     bash /app/config/createdb.sh;;
-	        "createadmin")  /app/config/createadmin;;
+	        "createdb")     bash /src/config/createdb.sh;;
+	        "createadmin")  /src/config/createadmin;;
 	    esac
   done
   
   if [[ -z $@ ]]; then
-      supervisord -c /app/config/supervisord.conf
+      supervisord -c /src/config/supervisord.conf
   fi
