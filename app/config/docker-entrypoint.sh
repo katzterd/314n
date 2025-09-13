52766a335c98eced2310654062c557fd8444b720
Vlad Kortnev <194586003+katzterd@users.noreply.github.com>
2025-09-14 02:10:36 +0300

    update app configs & k8s to upstream (#4)

--- app/config/docker-entrypoint.sh
+++ app/config/docker-entrypoint.sh
@@ -2,18 +2,18 @@
 
   if [ -z "${TORGATE_HOSTNAME}" ]; then
       echo "TORGATE_HOSTNAME environment var is undefined, onion header will be disabled";
-      sed -i 's/__TORGATE_HEADER__//' /src/config/nginx.conf
+      sed -i 's/__TORGATE_HEADER__//' /nginx.conf
   else
-      sed -i 's/__TORGATE_HEADER__/'"add_header Onion-Location http:\/\/${TORGATE_HOSTNAME}\$request_uri;"'/' /src/config/nginx.conf
+      sed -i 's/__TORGATE_HEADER__/'"add_header Onion-Location http:\/\/${TORGATE_HOSTNAME}\$request_uri;"'/' /nginx.conf
   fi
 
   for ARGS in $@; do
 	    case $ARGS in    
-	        "createdb")     bash /src/config/createdb.sh;;
-	        "createadmin")  /src/config/createadmin;;
+	        "createdb")     bash /createdb.sh;;
+	        "createadmin")  /createadmin;;
 	    esac
   done
   
   if [[ -z $@ ]]; then
-      supervisord -c /src/config/supervisord.conf
+      supervisord -c /supervisord.conf
   fi
